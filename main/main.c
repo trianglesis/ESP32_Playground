386b4daa067b1e1be5b29e00337d81cbd420a8aa
Oleksandr Danylchenko <sanekm2010@gmail.com>
2025-04-04 14:47:50 +0300

    first commit

---
+++ main/main.c
@@ -0,0 +1,245 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "driver/gpio.h"
+#include "led_strip.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "esp_random.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+// For ESP32 C6 with integrated LED
+// Init the LED as a usefull indicator of a working cycle.
+// In addition use 3v Beeper and one GPIO as a lightweight tester for PIN IOs =)
+
+// Do not used
+#define LED_STRIP_USE_DMA 0
+
+// LED OPTS
+#define TIME_TICK_MS 500
+
+#define LED_STRIP_LED_COUNT 1
+#define LED_STRIP_MEMORY_BLOCK_WORDS 0 // let the driver choose a proper memory block size automatically
+
+#define LED_STRIP_GPIO_PIN 8
+#define LED_STRIP_RMT_RES_HZ (10 * 1000 * 1000)
+
+static const char *TAG = "playground";
+
+// 
+// Experimental blocks for stuff
+// 
+void dump_all_gpio() {
+    // https://docs.espressif.com/projects/esp-idf/en/v5.4.1/esp32c6/api-reference/peripherals/gpio.html
+    gpio_dump_io_configuration(stdout, SOC_GPIO_VALID_GPIO_MASK);
+}
+
+void gpio_reset() {
+    // Testing all IOs at once
+    ESP_LOGI(TAG, "RESet all GPIOs");
+    gpio_reset_pin(GPIO_NUM_0); // Bip
+    gpio_reset_pin(GPIO_NUM_1); // Silent
+    gpio_reset_pin(GPIO_NUM_2); // Bip
+    gpio_reset_pin(GPIO_NUM_3); // Bip
+    gpio_reset_pin(GPIO_NUM_4); // Silent
+    gpio_reset_pin(GPIO_NUM_5); // Silent
+    gpio_reset_pin(GPIO_NUM_6); // Silent
+    gpio_reset_pin(GPIO_NUM_7); // Silent
+    // gpio_reset_pin(GPIO_NUM_8); // LED
+    gpio_reset_pin(GPIO_NUM_9); // Silent
+    gpio_reset_pin(GPIO_NUM_10); // Bip
+    gpio_reset_pin(GPIO_NUM_11); // Bip
+    gpio_reset_pin(GPIO_NUM_12); // Bip
+    gpio_reset_pin(GPIO_NUM_13); // Bip
+    // gpio_reset_pin(GPIO_NUM_14); // Not on board
+    gpio_reset_pin(GPIO_NUM_15); // Silent
+    // gpio_reset_pin(GPIO_NUM_16); // Not on board
+    // gpio_reset_pin(GPIO_NUM_17); // Not on board
+    gpio_reset_pin(GPIO_NUM_18); // Silent
+    gpio_reset_pin(GPIO_NUM_19); // Silent
+    gpio_reset_pin(GPIO_NUM_20); // Silent
+    gpio_reset_pin(GPIO_NUM_21); // Silent
+    gpio_reset_pin(GPIO_NUM_22); // Silent
+    gpio_reset_pin(GPIO_NUM_23); // Silent
+    // gpio_reset_pin(GPIO_NUM_24); // **RESERVED**
+    // gpio_reset_pin(GPIO_NUM_25); // **RESERVED**
+    // gpio_reset_pin(GPIO_NUM_26); // **RESERVED**
+    // gpio_reset_pin(GPIO_NUM_27); // Not on board
+    // gpio_reset_pin(GPIO_NUM_28); // **RESERVED**
+    // gpio_reset_pin(GPIO_NUM_29); // **RESERVED**
+    // gpio_reset_pin(GPIO_NUM_30); // **RESERVED**
+}
+
+void gpio_set_output() {
+    ESP_LOGI(TAG, "Set all not reserved GPIO direction to output");
+    gpio_set_direction(GPIO_NUM_0, GPIO_MODE_OUTPUT); // Bip
+    gpio_set_direction(GPIO_NUM_1, GPIO_MODE_OUTPUT); // Silent
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT); // Bip
+    gpio_set_direction(GPIO_NUM_3, GPIO_MODE_OUTPUT); // Bip
+    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT); // Silent
+    gpio_set_direction(GPIO_NUM_5, GPIO_MODE_OUTPUT); // Silent
+    gpio_set_direction(GPIO_NUM_6, GPIO_MODE_OUTPUT); // Silent
+    gpio_set_direction(GPIO_NUM_7, GPIO_MODE_OUTPUT); // Silent
+    // gpio_set_direction(GPIO_NUM_8, GPIO_MODE_OUTPUT); // LED
+    gpio_set_direction(GPIO_NUM_9, GPIO_MODE_OUTPUT); // Silent
+    gpio_set_direction(GPIO_NUM_10, GPIO_MODE_OUTPUT); // Bip
+    gpio_set_direction(GPIO_NUM_11, GPIO_MODE_OUTPUT); // Bip
+    gpio_set_direction(GPIO_NUM_12, GPIO_MODE_OUTPUT); // Bip
+    gpio_set_direction(GPIO_NUM_13, GPIO_MODE_OUTPUT); // Bip
+    // gpio_set_direction(GPIO_NUM_14, GPIO_MODE_OUTPUT); // Not on board
+    gpio_set_direction(GPIO_NUM_15, GPIO_MODE_OUTPUT); // Silent
+    // gpio_set_direction(GPIO_NUM_16, GPIO_MODE_OUTPUT); // Not on board
+    // gpio_set_direction(GPIO_NUM_17, GPIO_MODE_OUTPUT); // Not on board
+    gpio_set_direction(GPIO_NUM_18, GPIO_MODE_OUTPUT); // Silent
+    gpio_set_direction(GPIO_NUM_19, GPIO_MODE_OUTPUT); // Silent
+    gpio_set_direction(GPIO_NUM_20, GPIO_MODE_OUTPUT); // Silent
+    gpio_set_direction(GPIO_NUM_21, GPIO_MODE_OUTPUT); // Silent
+    gpio_set_direction(GPIO_NUM_22, GPIO_MODE_OUTPUT); // Silent
+    gpio_set_direction(GPIO_NUM_23, GPIO_MODE_OUTPUT); // Silent
+    // gpio_set_direction(GPIO_NUM_24, GPIO_MODE_OUTPUT); // **RESERVED**
+    // gpio_set_direction(GPIO_NUM_25, GPIO_MODE_OUTPUT); // **RESERVED**
+    // gpio_set_direction(GPIO_NUM_26, GPIO_MODE_OUTPUT); // **RESERVED**
+    // gpio_set_direction(GPIO_NUM_27, GPIO_MODE_OUTPUT); // Not on board
+    // gpio_set_direction(GPIO_NUM_28, GPIO_MODE_OUTPUT); // **RESERVED**
+    // gpio_set_direction(GPIO_NUM_29, GPIO_MODE_OUTPUT); // **RESERVED**
+    // gpio_set_direction(GPIO_NUM_30, GPIO_MODE_OUTPUT); // **RESERVED**
+}
+
+void gpio_set_high() {
+    ESP_LOGI(TAG, "Set all not reserved GPIO to HIGH");
+    gpio_set_level(GPIO_NUM_0, 1); // Bip
+    gpio_set_level(GPIO_NUM_1, 1); // Silent
+    gpio_set_level(GPIO_NUM_2, 1); // Bip
+    gpio_set_level(GPIO_NUM_3, 1); // Bip
+    gpio_set_level(GPIO_NUM_4, 1); // Silent
+    gpio_set_level(GPIO_NUM_5, 1); // Silent
+    gpio_set_level(GPIO_NUM_6, 1); // Silent
+    gpio_set_level(GPIO_NUM_7, 1); // Silent
+    // gpio_set_level(GPIO_NUM_8, 1); // LED
+    gpio_set_level(GPIO_NUM_9, 1); // Silent
+    gpio_set_level(GPIO_NUM_10, 1); // Bip
+    gpio_set_level(GPIO_NUM_11, 1); // Bip
+    gpio_set_level(GPIO_NUM_12, 1); // Bip
+    gpio_set_level(GPIO_NUM_13, 1); // Bip
+    // gpio_set_level(GPIO_NUM_14, 1); // Not on board
+    gpio_set_level(GPIO_NUM_15, 1); // Silent
+    // gpio_set_level(GPIO_NUM_16, 1); // Not on board
+    // gpio_set_level(GPIO_NUM_17, 1); // Not on board
+    gpio_set_level(GPIO_NUM_18, 1); // Silent
+    gpio_set_level(GPIO_NUM_19, 1); // Silent
+    gpio_set_level(GPIO_NUM_20, 1); // Silent
+    gpio_set_level(GPIO_NUM_21, 1); // Silent
+    gpio_set_level(GPIO_NUM_22, 1); // Silent
+    gpio_set_level(GPIO_NUM_23, 1); // Silent
+    // gpio_set_level(GPIO_NUM_24, 1); // **RESERVED**
+    // gpio_set_level(GPIO_NUM_25, 1); // **RESERVED**
+    // gpio_set_level(GPIO_NUM_26, 1); // **RESERVED**
+    // gpio_set_level(GPIO_NUM_27, 1); // Not on board
+    // gpio_set_level(GPIO_NUM_28, 1); // **RESERVED**
+    // gpio_set_level(GPIO_NUM_29, 1); // **RESERVED**
+    // gpio_set_level(GPIO_NUM_30, 1); // **RESERVED**
+}
+
+// Example redefining GPIO
+void gpio_config_pullup() {
+    // https://docs.espressif.com/projects/esp-idf/en/v5.4.1/esp32c6/api-reference/peripherals/gpio.html#configure-usb-phy-pins-to-gpio
+    gpio_config_t usb_phy_conf = {
+        .pin_bit_mask = (1ULL << 1) | (1ULL << 0),
+        .mode = GPIO_MODE_INPUT_OUTPUT,
+        .pull_up_en = 0,
+        .pull_down_en = 0,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&usb_phy_conf); // Bip
+}
+
+// 
+// Common blocks that are working and indicative
+// 
+led_strip_handle_t configure_led(void) {
+    // LED strip general initialization, according to your led board design
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = LED_STRIP_GPIO_PIN, // The GPIO that connected to the LED strip's data line
+        .max_leds = LED_STRIP_LED_COUNT,      // The number of LEDs in the strip,
+        .led_model = LED_MODEL_WS2812,        // LED strip model
+        .color_component_format = LED_STRIP_COLOR_COMPONENT_FMT_GRB, // The color order of the strip: GRB
+        .flags = {
+            .invert_out = false, // don't invert the output signal
+        }
+    };
+
+    // LED strip backend configuration: RMT
+    led_strip_rmt_config_t rmt_config = {
+        .clk_src = RMT_CLK_SRC_DEFAULT,        // different clock source can lead to different power consumption
+        .resolution_hz = LED_STRIP_RMT_RES_HZ, // RMT counter clock frequency
+        .mem_block_symbols = LED_STRIP_MEMORY_BLOCK_WORDS, // the memory block size used by the RMT channel
+        .flags = {
+            .with_dma = LED_STRIP_USE_DMA,     // Using DMA can improve performance when driving more LEDs
+        }
+    };
+
+    // LED Strip object handle
+    led_strip_handle_t led_strip;
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    ESP_LOGI(TAG, "Created LED strip object with RMT backend");
+    return led_strip;
+}
+
+int random_int_range(int min, int max) {    
+    // Return random int from the range.
+    // Use to blink led with random values.
+    return min + (esp_random() % (max - min + 1));
+}
+
+void led_control_pixel(int i, led_strip_handle_t led_strip) {
+    // Simple colour
+    int RED = random_int_range(1, 255);
+    int GREEN = random_int_range(1, 255);
+    int BLUE = random_int_range(1, 255);
+    ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, RED, GREEN, BLUE));
+}
+
+void led_control_hsv(int i, led_strip_handle_t led_strip) {
+    // Color with saturation and brightness
+    int hue = random_int_range(1, 360);  // MAX 360
+    int saturation = random_int_range(1, 255); // MAX 255
+    int brightLvl = random_int_range(1, 2);  // MAX 255
+    ESP_ERROR_CHECK(led_strip_set_pixel_hsv(led_strip, i, hue, saturation, brightLvl));
+}
+
+void beep_setup() {
+    // Clean the PIN and setup usage
+    gpio_reset_pin(GPIO_NUM_23);
+    gpio_set_direction(GPIO_NUM_23, GPIO_MODE_OUTPUT);
+    gpio_set_pull_mode(GPIO_NUM_23, GPIO_PULLUP_ONLY);
+    gpio_set_level(GPIO_NUM_23, 0);
+}
+
+void pin_beep(bool led_on_off) {
+    // Bip - simple beeper
+    if (led_on_off) {
+        gpio_set_level(GPIO_NUM_23, 1); // Bip
+    } else {
+        gpio_set_level(GPIO_NUM_23, 0); // Not Bip
+    }
+}
+
+//  Start BIP, start LED, cycle both
+void app_main(void)
+{
+    // Setup BIP pin
+    beep_setup();
+    // Led setup
+    led_strip_handle_t led_strip = configure_led();
+    bool led_on_off = false;
+    ESP_LOGI(TAG, "Start blinking LED and Beep");
+    while (1) {
+        // Led blink
+        led_blink(led_on_off, led_strip);
+        // Reset
+        led_on_off = !led_on_off;
+        // Sleep
+        vTaskDelay(pdMS_TO_TICKS(TIME_TICK_MS));
+    }
+}
